6f9d5e15d8bbb944b558c2c362d06328c3e3b73b
sumxu96 <sxu41@usfca.edu>
2018-09-11 19:02:04 -0700

    Edited main.cpp

--- NachenBlaster/main.cpp
+++ NachenBlaster/main.cpp
@@ -4,11 +4,7 @@
 #include <string>
 using namespace std;
 
-  // If your program is having trouble finding the Assets directory,
-  // replace the string literal with a full path name to the directory,
-  // e.g., "Z:/CS32/NachenBlaster/Assets" or "/Users/fred/cs32/NachenBlaster/Assets"
-
-const string assetDirectory = "/Users/SumXu/Documents/NachenBlaster/Assets"; 
+const string assetDirectory = ""; 
 
 class GameWorld;
 
